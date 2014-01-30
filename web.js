43da0bf41c0a51c3f419ae3178a79feb58f1d129
Eugene Marinelli <eugeneiiim@gmail.com>
2014-01-30 13:28:33 -0800

    get

--- web.js
+++ web.js
@@ -7,6 +7,10 @@ var app = express();
 app.use(express.urlencoded());
 app.use(express.json());
 
+app.get('/', function(req, res) {
+  res.send({x: true});
+});
+
 app.post('/test', function(req, res) {
   console.log('body:', req.body);
   res.send('hi');
